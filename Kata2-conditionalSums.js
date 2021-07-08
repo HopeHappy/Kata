cc5ca92bda235b42b288f15bbda40f1bfa2b7eb0
HopeHappy <hope_happy@outlook.com>
2021-07-08 15:26:34 -0400

    Kata2

---
+++ Kata2-conditionalSums.js
@@ -0,0 +1,24 @@
+const conditionalSum = (values, condition) => {
+    let total = 0;
+
+    if (condition === 'odd') {
+        for (let value of values) {
+            if (value % 2 === 1 || value % 2 === -1) {
+                total += value;
+            }
+        }
+    } else if (condition === 'even') {
+        for (let value of values) {
+            if (value % 2 === 0) {
+                total += value;
+            }
+        }
+    }
+
+    return total;
+}
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
